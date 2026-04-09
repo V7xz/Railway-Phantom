3f3bd141c3a347e8b2bac64cba84508f4c2f6c45
V7xz <fachri.yusuf0701@gmail.com>
2026-04-10 04:29:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -403,6 +403,11 @@ const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
 (async () => {
   try {
+    console.log("🔄 Clearing old commands...");
+    await rest.put(
+      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+      { body: [] }
+    );
     console.log("🔄 Registering slash commands...");
     await rest.put(
       Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
